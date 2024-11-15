60f31bd6409ce577833835f06e6d8e2bfb29195b
LIEGEOIS Pierre-Yves <liegeoispy@gmail.com>
2024-11-15 11:11:42 +0100

    fix: use correct path to the zip
    Signed-off-by: LIEGEOIS Pierre-Yves <liegeoispy@gmail.com>

--- main.tf
+++ main.tf
@@ -84,7 +84,7 @@ resource "aws_lambda_function" "test_lambda" {
   role             = aws_iam_role.function_role.arn
   handler          = "bootstrap"
   runtime          = "go1.x"
-  filename         = "functions/first-function/src/lambda.zip"
+  filename         = "functions/first-function/src/bin/lambda.zip"
   source_code_hash = data.archive_file.lambda.output_base64sha256
 }
 
