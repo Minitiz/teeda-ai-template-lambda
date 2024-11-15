6cd1a5485cea2c3375f5e952faad0ee31fe773b3
LIEGEOIS Pierre-Yves <liegeoispy@gmail.com>
2024-11-15 11:23:02 +0100

    fix: use the correct runtime for golang application
    Signed-off-by: LIEGEOIS Pierre-Yves <liegeoispy@gmail.com>

--- main.tf
+++ main.tf
@@ -83,7 +83,7 @@ resource "aws_lambda_function" "test_lambda" {
   function_name    = "HelloFunction-${random_string.random.id}"
   role             = aws_iam_role.function_role.arn
   handler          = "bootstrap"
-  runtime          = "provided.al2023"
+  runtime          = "provided"
   filename         = "functions/first-function/src/bin/lambda.zip"
   source_code_hash = data.archive_file.lambda.output_base64sha256
 }
